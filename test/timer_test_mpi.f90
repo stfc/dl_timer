36b2a87606324563406bfddf8bbe503b540c06fc
Andrew Porter <andrew.porter@stfc.ac.uk>
2016-03-21 16:44:09 +0000

    #1 Add beginnings of new mpi-based test code

---
+++ test/timer_test_mpi.f90
@@ -0,0 +1,68 @@
+!> Simple program to test the timing API
+PROGRAM timer_test
+  use dl_timer
+  use mpi
+  implicit none
+
+  integer, parameter :: r_def = KIND(1.0d0)
+
+  integer :: time0, time1
+  integer :: istep
+  integer, parameter :: nstep = 1000
+  integer :: nloops
+
+  real(r_def) :: mysum
+
+  integer numtasks, rank, ierr, rc, len, i
+  character*(MPI_MAX_PROCESSOR_NAME) name
+
+  !--------------------------------------------------------------
+  ! Initialisation
+
+  ! Initialize the MPI library:
+  call MPI_INIT(ierr)
+  if (ierr .ne. MPI_SUCCESS) then
+     print *,'Error starting MPI program. Terminating.'
+     call MPI_ABORT(MPI_COMM_WORLD, rc, ierr)
+  end if
+
+  ! Get the number of processors this job is using:
+  call MPI_COMM_SIZE(MPI_COMM_WORLD, numtasks, ierr)
+
+  ! Get the rank of the processor this thread is running on.  (Each
+  ! processor has a unique rank.)
+  call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierr)
+
+  call timer_init()
+  mysum = 0.0d0
+  nloops = 200
+
+  !--------------------------------------------------------------
+  ! Time-stepping
+
+  call timer_start('Time-stepping', time0, nstep)
+
+  do istep = 1, nstep
+
+     call timer_start('Fake section', time1)
+     do j = 1, nloops
+        mysum = mysum + sqrt(5.0d0*istep*istep)
+     end do
+     call timer_stop(time1)
+
+  end do
+
+  call timer_stop(time0)
+
+  !---------------------------------------------------------------
+  ! Finalise
+
+  ! Output the result to prevent the compiler optimising-out the loop
+  write (*,*) 'Fake checksum = ', mysum
+
+  call timer_report()
+
+  ! Tell the MPI library to release all resources it is using:
+  call MPI_FINALIZE(ierr)
+
+END PROGRAM timer_test
