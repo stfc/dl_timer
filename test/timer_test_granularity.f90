27c087d2d29239f113449ff02e54d2cba07f9c95
Andrew Porter <andrew.porter@stfc.ac.uk>
2016-04-21 21:25:06 +0100

    #3 Tidying of granularity test code

--- test/timer_test_granularity.f90
+++ test/timer_test_granularity.f90
@@ -1,12 +1,13 @@
 !> Simple program to test the granularity/overhead of the timing API
+!! This code should be run pinned to a single core and for a duration
+!! sufficient to ensure the OS has migrated other processes off the
+!! chose core.
 PROGRAM timer_test_granularity
   use dl_timer
 
-  integer, parameter :: r_def = KIND(1.0d0)
-
   integer :: time0
   integer :: istep
-  integer, parameter :: nstep = 10000
+  integer, parameter :: nstep = 50000
 
   !--------------------------------------------------------------
   ! Initialisation
@@ -17,7 +18,8 @@ PROGRAM timer_test_granularity
   ! Time-stepping
 
   do istep = 1, nstep
-     call timer_start('Time-step', time0)
+     call timer_start('Empty time-step', time0)
+     ! Absolutely no work here!
      call timer_stop(time0)
   end do
 
