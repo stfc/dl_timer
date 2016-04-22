902ad2061d778cd61f7d0dab28bf7928d4cff1cd
Andrew Porter <andrew.porter@stfc.ac.uk>
2016-04-22 13:53:24 +0100

    #3 Add code (from John McCalpin) to get rdtscp

--- src/intel_timer.c
+++ src/intel_timer.c
@@ -10,7 +10,7 @@ int rdtsc_available(void)
 #endif
 }
 
-/* Timer for use on Intel chips. Results are only for inter-comparison
+/** Timer for use on Intel chips. Results are only for inter-comparison
    and not for conversion into some human measure of time.
    See http://en.wikipedia.org/wiki/Time_Stamp_Counter */
 uint64_t getticks(void)
@@ -18,13 +18,7 @@ uint64_t getticks(void)
     uint32_t lo, hi;
 
     /* We can use the Intel intrinsic __rdtsc() when building with
-       the Intel compiler so don't need the below */
-
-    /* We cannot use "=A", since this would use %rax on x86_64 and 
-       return only the lower 32bits of the TSC *
-    __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
-    return (uint64_t)hi << 32 | lo;*/
-
+       the Intel compiler */
 #if defined __INTEL_COMPILER
     return __rdtsc();
 #else
@@ -32,4 +26,18 @@ uint64_t getticks(void)
 	    "not compiled with the Intel compiler\n");
     return 0;
 #endif
+}
+
+/** Obtain the clock count using rdtscp instead of rdtsc. This is
+  better than rdtsc since it ensures synchronisation of out-of-order
+  instructions. This code provide by John D. McCalpin. */
+unsigned long tacc_rdtscp(int *chip, int *core)
+{
+  unsigned a, d, c;
+
+  __asm__ volatile("rdtscp" : "=a" (a), "=d" (d), "=c" (c));
+  *chip = (c & 0xFFF000)>>12;
+  *core = c & 0xFFF;
+
+  return ((unsigned long)a) | (((unsigned long)d) << 32);;
 }
