59b9ccd312655c02a2630725ee32bf4a520f10d8
Andrew Porter <andrew.porter@stfc.ac.uk>
2016-05-13 11:43:44 +0100

    #5 Put back OpenMP and MPI options in intel_setup.sh

--- compiler_setup/intel_setup.sh
+++ compiler_setup/intel_setup.sh
@@ -2,8 +2,8 @@
 # Environment vars set-up in order to build dl_timer with the Intel compiler
 export F90=ifort
 export F90FLAGS="-fast"
-export LDFLAGS="-fast"
+export LDFLAGS=
 export CC=icc
-#export MPIF90=mpiifort
-#export OMPFLAGS="-openmp"
+export MPIF90=mpiifort
+export OMPFLAGS="-openmp"
 
